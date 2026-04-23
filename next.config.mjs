77e705ead497ee819f8168ab0bd8c19c48663f18
neilmcardle <nmcardle2016@gmail.com>
2026-04-23 14:47:36 +0100

    Updating CSP Error

--- next.config.mjs
+++ next.config.mjs
@@ -69,7 +69,7 @@ const nextConfig = {
                   key: 'Content-Security-Policy',
                   value:
                     "default-src 'self'; " +
-                    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com; " +
+                    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://cdn.jsdelivr.net; " +
                     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
                     "img-src 'self' data: blob: https:; " +
                     "font-src 'self' data: https://fonts.gstatic.com; " +
