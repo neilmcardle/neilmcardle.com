da708f4698f3cf63d7f141fcb4b0d3dcf04d37f9
neilmcardle <nmcardle2016@gmail.com>
2026-09-08 10:14:18 +0100

    Replace ProfileCoin with hero portrait
    Render a heroBadge using next/image.

--- components/home/HomeShell.tsx
+++ components/home/HomeShell.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import styles from "./home.module.css";
 import Image from "next/image";
 import DotField from "./DotField";
-import ProfileCoin from "./ProfileCoin";
 import LiveSentence from "./LiveSentence";
 import SelectedWork from "./SelectedWork";
 import SiteMenu from "./SiteMenu";
@@ -30,7 +29,16 @@ export default function HomeShell() {
 
           <div className={styles.profile}>
             <div className={styles.profileTop}>
-              <ProfileCoin />
+              <div className={styles.heroBadge}>
+                <Image
+                  src="/hero/portrait.png"
+                  alt="Neil McArdle"
+                  width={480}
+                  height={480}
+                  sizes="124px"
+                  priority
+                />
+              </div>
 
               <div className={styles.profileMeta}>
                 <h1 className={styles.profileName}>Neil McArdle</h1>
